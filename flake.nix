50a2ae40b41ef5a5db1abfae099861ac0a0742e9
Will Bush <will.g.bush@gmail.com>
2024-01-20 18:52:11 -0600

    follow nixos-unstable and home-manager unstable

--- flake.nix
+++ flake.nix
@@ -2,11 +2,8 @@
   description = "Your new nix config";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
